0bd8ed168faf0eaae1499929bed1e862a9fe378b
Nemesis004 <93238544+Nemesis004@users.noreply.github.com>
2021-10-28 03:32:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#Made By Némesis
+#Date Creation: 28/10/2021
+
+import socket
+from colorama import Fore, init
+import os
+import time
+init()
+
+os.system("cls")
+
+print(Fore.GREEN + "Buscando Información..")
+time.sleep(7)
+
+os.system("cls")
+
+print(Fore.GREEN + "¡Información Encontrada!")
+
+pc = socket.gethostname() 
+
+ip = socket.gethostbyname(pc) 
+
+print("")
+print(Fore.WHITE + "El nombre de tu PC es: " + Fore.RED + pc) 
+print(Fore.WHITE + "La IP de tu PC es: " + Fore.RED + ip)
+print("")
+print(Fore.GREEN + "¡Un saludo crack!")
